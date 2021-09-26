d754c8ef4ad13da11385645c46f32b253c13d4d7
thelaserunicorn <rashideram2@gmail.com>
2021-09-26 19:42:19 +0600

    added delete task method

--- task_manager_api/controllers/tasks.js
+++ task_manager_api/controllers/tasks.js
@@ -1,14 +1,14 @@
 const Task = require("../models/Task")
 
 
-const getAllTasks = (req, res) => {
-    res.send("all items from list")
+const getAllTasks = async(req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
-
-const getTask = (req, res) => {
-    res.json({id: req.params.id})
-}
-
 const createTask = async(req, res) => {
 
     try {
@@ -20,8 +20,32 @@ const createTask = async(req, res) => {
     
 }
 
-const deleteTask = (req, res) => {
-    res.json({id: req.params.id})
+const getTask = async(req, res) => {
+    try {
+        const{id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg: `no task with id ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+}
+
+
+const deleteTask = async(req, res) => {
+    try {
+        const{id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg: `no task with id ${taskID}`})
+        }
+        res.status(200).json({task})
+
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
 const updateTask = (req, res) => {
