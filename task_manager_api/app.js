186660dc0e537738cb88d22d637d9ea5c4909e92
thelaserunicorn <rashideram2@gmail.com>
2021-09-26 17:58:36 +0600

    added .env

--- task_manager_api/app.js
+++ task_manager_api/app.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const tasks = require("./routes/tasks")
-
+const connectDB = require("./db/connect")
+require('dotenv').config()
 //middleware
 app.use(express.json())
 
@@ -14,4 +15,17 @@ app.use('/api/v1/tasks', tasks)
 
 
 const port = 3000
-app.listen(port, console.log(`listening in port ${port}`))
+
+const start = async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`listening in port ${port}`))
+        console.log("DB CONNECTED")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+start()
+
