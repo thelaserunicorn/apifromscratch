582049f7aa9af62965d14623c2a3f9f9ceb323d3
thelaserunicorn <rashideram2@gmail.com>
2021-09-27 13:16:23 +0600

    created mongoose model

--- store_api/models/product.js
+++ store_api/models/product.js
@@ -8,5 +8,26 @@ const productSchema = new mongoose.Schema({
     price:{
         type: Number,
         required: [true, 'product price must be provided']
+    },
+    featured:{
+        type: Boolean,
+        default: false
+    },
+    rating:{
+        type: Number,
+        default: 4.5
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    company:{
+        type: String,
+        enum:{
+         values: ['ikea', 'liddy', 'caressa', 'marcos'],
+         message: '{VALUE} is not supported'
+        } 
     }
 })
+
+module.exports = mongoose.model('Product', productSchema)
