088c886a89e82d1ef5ff238112c60f3737689e71
thelaserunicorn <rashideram2@gmail.com>
2021-10-10 09:57:15 +0600

    added-db

--- basic_crud_api_mongo/index.js
+++ basic_crud_api_mongo/index.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 const PORT = 3000
+require('dotenv/config')
 
 app.use(express.json())
 app.get("/", (req,res)=>{
     res.json({success: "true"})
 })
 
+
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true},()=>{
+    console.log("db connected")
+})
 app.listen(PORT,()=>{console.log(`listening in port ${PORT}`)})
